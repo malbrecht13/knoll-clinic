59ea5321832029ca2415d9b7e4ed35aad3633ca2
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2021-04-20 04:14:38 -0500

    Changes Knoll building to jpg

--- components/Main/main.js
+++ components/Main/main.js
@@ -7,11 +7,9 @@ export default function Main() {
 
     
     return (
-        <main >
           <Addressbanner/>
           <HeroImage/>
           <PhoneNumber />
           <Testimonial />
-        </main>
     );
 }
